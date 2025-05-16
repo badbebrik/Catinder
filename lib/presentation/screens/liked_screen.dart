b80ca6ef354c06846f08c3878e23b879ea18d7ee
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2025-05-16 21:36:35 +0300

    Extract liked_card

--- lib/presentation/screens/liked_screen.dart
+++ lib/presentation/screens/liked_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../../../domain/entities/liked_cat.dart';
 import '../cubits/liked_cats_cubit.dart';
+import '../widgets/liked_card.dart';
 import 'detail_screen.dart';
 
 class LikedScreen extends StatelessWidget {
@@ -106,7 +107,7 @@ class LikedScreen extends StatelessWidget {
                   : SliverList.builder(
                 itemCount: loaded.cats.length,
                 itemBuilder: (_, i) =>
-                    _LikedCard(liked: loaded.cats[i]),
+                    LikedCard(liked: loaded.cats[i]),
               ),
             ],
           );
@@ -116,98 +117,3 @@ class LikedScreen extends StatelessWidget {
   }
 }
 
-class _LikedCard extends StatelessWidget {
-  final LikedCat liked;
-  const _LikedCard({required this.liked});
-
-  @override
-  Widget build(BuildContext context) {
-    final cubit = context.read<LikedCatsCubit>();
-    final breedName = liked.cat.breed?.name ?? 'Cat';
-    final likedAtString = liked.likedAt.toLocal().toString().substring(0, 16);
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      child: GestureDetector(
-        onTap: () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => DetailScreen(cat: liked.cat)),
-        ),
-        child: Card(
-          elevation: 6,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
-          ),
-          clipBehavior: Clip.antiAlias,
-          child: SizedBox(
-            height: 160,
-            child: Row(
-              children: [
-                CachedNetworkImage(
-                  imageUrl: liked.cat.imageUrl,
-                  width: 160,
-                  height: 160,
-                  fit: BoxFit.cover,
-                  placeholder: (_, __) => Container(
-                    color: Colors.grey[200],
-                    child: const Center(child: CircularProgressIndicator()),
-                  ),
-                  errorWidget: (_, __, ___) => const Icon(Icons.error),
-                ),
-
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 12,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              breedName,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            const SizedBox(height: 4),
-                            Text(
-                              'Liked $likedAtString',
-                              style: const TextStyle(color: Colors.grey),
-                            ),
-                          ],
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: IconButton(
-                            onPressed: () => cubit.delete(liked.cat.id),
-                            icon: const Icon(Icons.delete_outline),
-                            color: Colors.red,
-                            iconSize: 24,
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(
-                              minWidth: 24,
-                              minHeight: 24,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
